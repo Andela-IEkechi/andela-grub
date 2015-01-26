37499737857220e2d289d6c80241cc6f1c187b64
Andela-MonsieurSlim <luccy559slim@gmail.com>
2015-01-26 08:30:04 +0100

    removed usermailer temporarily

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
-class UserMailer < ActionMailer::Base
-  default from: "reminders@example.com"
-
-  def user_reminder
-    @user = User.find(2)
-    @url = 'http://andela-grub.herokuapp.com'
-    mail(to: @user.email, subject: 'Please place your meal orders for tomorrow')
-  end
-
-  def admin_reminder
-    @user = User.where(admin: true)
-    @url = 'http://andela-grub.herokuapp.com'
-    mail(to: @user.email, subject: 'Please create meal options for tomorrow')
-  end
-end
+# class UserMailer < ActionMailer::Base
+#   default from: "reminders@example.com"
+#
+#   def user_reminder
+#     @user = User.find(2)
+#     @url = 'http://andela-grub.herokuapp.com'
+#     mail(to: @user.email, subject: 'Please place your meal orders for tomorrow')
+#   end
+#
+#   def admin_reminder
+#     @user = User.where(admin: true)
+#     @url = 'http://andela-grub.herokuapp.com'
+#     mail(to: @user.email, subject: 'Please create meal options for tomorrow')
+#   end
+# end
--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,21 +1,21 @@
-require 'rufus-scheduler'
-
-scheduler = Rufus::Scheduler.new
-
-# Send the digest every day at noon
-scheduler.cron("0 12 * * *") do
-  UserMailer.user_reminder.deliver()
-end
-
-scheduler = Rufus::Scheduler.new
-
-# Send the digest every day at noon
-scheduler.cron("0 12 * * *") do
-  UserMailer.admin_reminder
-end
-
-scheduler = Rufus::Scheduler.new
-
-scheduler.every("1m") do
-  UserMailer.user_reminder.deliver()
-end
+# require 'rufus-scheduler'
+#
+# scheduler = Rufus::Scheduler.new
+#
+# # Send the digest every day at noon
+# scheduler.cron("0 12 * * *") do
+#   UserMailer.user_reminder.deliver()
+# end
+#
+# scheduler = Rufus::Scheduler.new
+#
+# # Send the digest every day at noon
+# scheduler.cron("0 12 * * *") do
+#   UserMailer.admin_reminder
+# end
+#
+# scheduler = Rufus::Scheduler.new
+#
+# scheduler.every("1m") do
+#   UserMailer.user_reminder.deliver()
+# end
